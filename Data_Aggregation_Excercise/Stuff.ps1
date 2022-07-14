63c9073e3544d00af8837591b614d7dac282bcbd
Elijah Robinson <EM.Robinson109@gmail.com>
2022-07-14 18:52:32 -0400

    Create Stuff.ps1

---
+++ Data_Aggregation_Excercise/Stuff.ps1
@@ -0,0 +1,27 @@
+﻿$Data = Import-Csv "C:\Users\AAP8801\MOCK_DATA.csv"
+
+$Departments = $Data | Select-Object Department -Unique
+$DepartmentObject = @()
+
+foreach($Department in $Departments){
+
+$DepartmentData = $Data | Where-Object {$_.Department -eq $Department.department}
+$DepartmentSum = [Decimal]0
+
+Foreach($Branch in $DepartmentData){
+
+$BranchCost = [Decimal]$Branch.cost.Replace('$','')
+$DepartmentSum = $DepartmentSum + $BranchCost
+
+}
+
+$DepartmentObject += New-Object -TypeName PSObject -Property @{
+
+        'Department' = $Department.department
+        'TotalCost' = $DepartmentSum
+        
+    }
+
+}
+
+$DepartmentObject | Export-Csv -Path "C:\Users\AAP8801\MOCK_DATA_TOTAL.csv" -NoTypeInformation -Force
