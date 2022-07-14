e6c29a182dd19f7f46cccb0daf73f89bc304b1e1
Elijah Robinson <105064587+EM-Robinson@users.noreply.github.com>
2022-07-14 19:32:40 -0400

    Update DataAggregationExcercise.ps1

--- Data_Aggregation_Excercise/DataAggregationExcercise.ps1
+++ Data_Aggregation_Excercise/DataAggregationExcercise.ps1
@@ -1,7 +1,16 @@
-﻿$Data = Import-Csv "C:\Users\AAP8801\MOCK_DATA.csv"
+#Importing the MOCK_DATA.csv and assigning the data to $Data
+
+$Data = Import-Csv "C:\Users\AAP8801\MOCK_DATA.csv"
+
+<#Selecting only the department property of each object in $Data. The -Unique parameter allows us to grab only one instance of each department so there are not grabbing duplicate departments in the data we've filtered. This data is stored into $Departments#>
 
 $Departments = $Data | Select-Object Department -Unique
+
+#Initialing an array object where our new data will be stored.
+
 $DepartmentObject = @()
+
+
 
 foreach($Department in $Departments){
 
